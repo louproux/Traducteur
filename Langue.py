b1dfb3e27fa225a371be805b771215d5cc1ad8b8
Theo_Berteloot <theo.bertelootstdo@laposte.net>
2019-06-01 11:20:54 +0200

    first commit

---
+++ Langue.py
@@ -0,0 +1,8 @@
+""" Class : Langue
+    Paramètres : 
+        - separateur : indique le separateur entre chaque mots de la langue
+"""
+
+class Langue:
+    def __init__(self,_separateur):
+        self.separateur = _separateur
